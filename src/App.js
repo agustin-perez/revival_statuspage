3f5269cbdbb636802f7752ab0d6ad89fc26e5065
agustin-perez <agperez.prieto@gmail.com>
2021-02-24 06:54:00 -0300

    beta release

--- src/App.js
+++ src/App.js
@@ -11,20 +11,21 @@ class App extends Component {
     isLoading: true
   }
 
-  
-
   async componentDidMount() {
-    const endpointURL = "https://sonic.runner.es/statusAPI/generate204";
-    let response = await fetch(endpointURL);
-    console.log(response.status);
-    if (response.status === 204){
+    const outrunEndpointURL = "https://sonic.runner.es/statusAPI/generate204";
+    const yackerEndpointURL = "https://sonic.runner.es/botLogAPI/logs/";
+    let outrunPromise = await fetch(outrunEndpointURL);
+    let yackerPromise = await fetch(yackerEndpointURL);
+    if (outrunPromise.status === 204){
       this.setState({outrunStatus: true, isLoading: false});
+    }
+    if (yackerPromise.status === 404){
+      this.setState({yackerStatus: true, isLoading: false});
     }
     this.setState({isLoading: false});
   }
 
   render(){
-    //TODO: agregar componentes individuales para cada bot
     if(this.state.isLoading){
       return(
           <div className="App">
@@ -39,8 +40,7 @@ class App extends Component {
           <h2>Server</h2>
           <StatusCard title={(this.state.outrunStatus ? ("Outrun") : ("Outrun ⚠"))} text={(this.state.outrunStatus ? ("Working") : ("Server down"))} severity={(this.state.outrunStatus ? ("default") : ("critical"))}/>
           <h2>Services</h2>
-          <StatusCard title={(this.state.yackerStatus ? ("Yacker") : ("Yacker ⚠"))} text={(this.state.yackerStatus ? ("Working") : ("Unknown"))} severity={(this.state.yackerStatus ? ("default") : ("warning"))}/>
-          <StatusCard title={(this.state.milesStatus ? ("The Miles Electric") : ("The Miles Electric ⚠"))} text={(this.state.milesStatus? ("Working") : ("Unknown"))} severity={(this.state.milesStatus ? ("default") : ("warning"))}/>
+          <StatusCard title={(this.state.yackerStatus ? ("Yacker") : ("Yacker ⚠"))} text={(this.state.yackerStatus ? ("Working") : ("Bot not available"))} severity={(this.state.yackerStatus ? ("default") : ("critical"))}/>
         </div>
         <p>Welcome to the Revival Status Page.<br/>All the content shown on this page depends on the actual server to be available. 
         If this page is not loading, please let us know at our Discord server.</p>
